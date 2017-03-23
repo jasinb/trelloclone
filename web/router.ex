3c8d026bdb1f6a7a70d61547d825db270b50c1a8
Jasin Bushnaief <jasin@umbra3d.com>
2017-03-23 12:37:29 +0200

    Start frontend

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,7 @@ defmodule Trelloclone.Router do
   scope "/", Trelloclone do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
