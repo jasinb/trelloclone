a5655f8c7c81861ccb3ab3b6184440f852371ba6
Jasin Bushnaief <jasin@umbra3d.com>
2017-03-24 09:39:07 +0200

    Migrate boards

---
+++ web/models/board.ex
@@ -0,0 +1,19 @@
+defmodule Trelloclone.Board do
+  use Trelloclone.Web, :model
+
+  schema "boards" do
+    field :name, :string
+    belongs_to :user, Trelloclone.User
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name])
+    |> validate_required([:name])
+  end
+end
